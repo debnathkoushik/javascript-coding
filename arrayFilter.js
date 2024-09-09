3fdef68e6eedf9a30abe91d83fa8b2ba9cbc62e3
Koushik Debnath <koushikdebnath@192.168.68.106>
2024-09-10 00:00:08 +0530

    fix implement of filter method of array

---
+++ arrayFilter.js
@@ -0,0 +1,19 @@
+/**
+ Designing Array.prototype.filter
+ */
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+
+    let result = [];
+  
+    for (let index = 0; index < this.length; index++){
+      const eachValue = this[index];
+  
+      if (Object.hasOwn(this, index) && callbackFn.call(thisArg, eachValue, index, this)){
+        result.push(eachValue)
+      }
+    }
+  
+    return result;
+    
+    throw 'Not implemented!';
+};
