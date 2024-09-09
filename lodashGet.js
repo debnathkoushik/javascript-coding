9bc1c6c0a81f7b183deb3e144712e44a54ee7434
Koushik Debnath <koushikdebnath@192.168.68.106>
2024-09-09 20:51:40 +0530

    fix implement of get method of lodash

---
+++ lodashGet.js
@@ -0,0 +1,28 @@
+/**
+ Designing Lodash's Get method
+ */
+
+export default function lodashGet(objectParam, pathParam, defaultValue) {
+
+    // check if pathParam is a an array
+    // if not, convert pathParam to an array
+    const pathArray = Array.isArray(pathParam) ? pathParam : String(pathParam).split(".");
+    
+    let index = 0;
+  
+    // check if data exists in the provided path
+    while (objectParam != null && index < pathArray.length){
+      objectParam = objectParam[pathArray[index]];
+      index++;
+    }
+  
+    // check if the provided path i.e. pathParam is traversed
+    // if not, return undefined
+    const returnedValue = index == pathArray.length ? objectParam : undefined;
+
+    // if the value provided after traversing pathParam is 'undefined'
+    // then, return the defaultValue
+    return returnedValue !== undefined ? returnedValue : defaultValue
+  
+    throw 'Not implemented!';
+}
