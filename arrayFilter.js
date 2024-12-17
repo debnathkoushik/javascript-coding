be186b1ad6d08d0c4fa085959db5f523a3156293
Koushik Debnath <debnathkoushik605@gmail.com>
2024-12-17 20:06:29 +0530

    fix formatting

--- arrayFilter.js
+++ arrayFilter.js
@@ -3,18 +3,20 @@
  */
 
 Array.prototype.myFilter = function (callbackFn, thisArg) {
+  let result = [];
 
-    let result = [];
-  
-    for (let index = 0; index < this.length; index++){
-      const eachValue = this[index];
-  
-      if (Object.hasOwn(this, index) && callbackFn.call(thisArg, eachValue, index, this)){
-        result.push(eachValue)
-      }
+  for (let index = 0; index < this.length; index++) {
+    const eachValue = this[index];
+
+    if (
+      Object.hasOwn(this, index) &&
+      callbackFn.call(thisArg, eachValue, index, this)
+    ) {
+      result.push(eachValue);
     }
-  
-    return result;
-    
-    throw 'Not implemented!';
+  }
+
+  return result;
+
+  throw "Not implemented!";
 };
